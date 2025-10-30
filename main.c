53d472d6c4bb3b32577a5a3754069aa8eb9095e6
shopia1122 <shopia123@sookmyung.ac.kr>
2025-10-30 16:06:26 +0900

    main_3_2

--- main.c
+++ main.c
@@ -5,29 +5,25 @@
 
 int main(int argc, char *argv[])
 {
-  //SIZE 크기 배열 선언   
-  int grade[SIZE]; 
-  int score[SIZE];   
-  int i;            
-  
-  for(i=0;i<SIZE;i++) 
-  {
-   grade[i] = rand()%100;
-  }
-  
-   for(i=0;i<SIZE;i++)    //배열도 반복해서 같다고 두어야 한다.  
-  {
-   score[i] = grade [i];
-  }
-  
-  
-  for(i=0;i<SIZE;i++) 
-   printf("grade[%d] = %d, score[%d] = %d\n", i, grade[i], i, score[i]);
+   int i;
+   int a[SIZE] = {1, 2, 3, 4, 5};
+   int b[SIZE] = {1, 2, 3, 4, 6};
+   int flag_same = 1;
 
+
+  for(i=0;i<SIZE;i++)
+  {
+     if(a[i] != b[i])
+       printf("%i-th element is diff.\n", i);     
+       flag_same = 0;  
+                   }
+                      
+  
+  
+  printf("Are a and b the same? %i\n", flag_same);
+  
   system("PAUSE");	
   return 0;
 }
 
 
-
-      
