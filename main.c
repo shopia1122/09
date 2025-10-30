f6d2cd5274c81a75e1488a7c1758a0a401dae9fa
shopia1122 <shopia123@sookmyung.ac.kr>
2025-10-30 16:12:55 +0900

    main4

--- main.c
+++ main.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5   
-// 몇 명인지를 매크로로 정의하는 것. 학생 명수를 바꿀 때도 SIZE 뒤의 5를 다른 숫자로 바꾸면 되므로 간편  
+#define SIZE 4  
+
+void square_array(int a[], int size);
+void print_array(int a[], int size);
+
 
 int main(int argc, char *argv[])
 {
-   int i;
-   int a[SIZE] = {1, 2, 3, 4, 5};
-   int b[SIZE] = {1, 2, 3, 4, 6};
-   int flag_same = 1;
-
-
-  for(i=0;i<SIZE;i++)
-  {
-     if(a[i] != b[i])
-       printf("%i-th element is diff.\n", i);     
-       flag_same = 0;  
-                   }
-                      
+    int list[SIZE] = {1, 2, 3, 4};
+    
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
   
-  
-  printf("Are a and b the same? %i\n", flag_same);
   
   system("PAUSE");	
   return 0;
+}
+
+void square_array(int a[], int size)
+{
+     int i;
+     
+     for(i=0;i<size;i++)
+      a[i]=a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+     int i;
+     
+     for(i=0;i<size;i++)
+      printf("%3d", a[i]);
+      printf("\n");
 }
 
 
