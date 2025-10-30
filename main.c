adb459769518d2ba26b1980a03eb74a223e2fa08
shopia1122 <shopia123@sookmyung.ac.kr>
2025-10-30 15:35:45 +0900

    main2_1

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5   
+// 몇 명인지를 매크로로 정의하는 것. 학생 명수를 바꿀 때도 SIZE 뒤의 5를 다른 숫자로 바꾸면 되므로 간편  
 
 int main(int argc, char *argv[])
 {
   //5명의 학생에 대한 시험 점수 저장 배열 선언 
-  int score[5];   
+  int score[SIZE];   
   int i;         // 점수를 출력하기 위한 변수 선언     
   
-  //점수 입력   
-  score[0] = 10;    //다섯 학생의 각각의 점수를 배열에 각각 저장 
-  score[1] = 20;
-  score[2] = 30;
-  score[3] = 40;
-  score[4] = 50;
+  printf("5명의 점수를 입력하세요.\n");
+  
+  //점수 입력 함수(반복문 활용) 
+  for(i=0;i<SIZE;i++)
+  scanf("%d", &score[i]);
   
   
   //점수 화면 출력(반복문 활용)   
-  for(i=0;i<5;i++)                                 // 반복을 위한 for문, n번 반복 원하면 5 대신 n을 대입   
+  for(i=0;i<SIZE;i++)                                 // 반복을 위한 for문 
   printf("grade[%d] = %d\n", i, score[i]);  
 
   system("PAUSE");	
